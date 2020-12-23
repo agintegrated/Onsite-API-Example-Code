0c92af83a3426cfe61a6820b15e86182d365863b
ngensimore <ngensimore@agintegrated.com>
2020-12-23 12:49:42 -0500

    #81230 comment: fixing a model

--- c#/Onsite-API-Example-Code/Models/Response/GetTreeResponse.cs
+++ c#/Onsite-API-Example-Code/Models/Response/GetTreeResponse.cs
@@ -10,7 +10,7 @@ namespace Onsite_API_Example_Code.Models.Response
         public bool Success { get; set; }
         public string Message { get; set; }
         public int Count { get; set; }
-        public List<TelematicsNodeResponse> TelematicsNode { get; set; }
+        public List<TelematicsNodeResponse> Data { get; set; }
     }
 
 }
