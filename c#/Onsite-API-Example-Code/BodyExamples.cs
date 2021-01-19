f452e60214d847eb0f9780eae66bf53a1737e3e1
Kevin Means <kmeans@agintegrated.com>
2021-01-19 11:44:43 -0500

    #81230 comment: Updated a Geometry model to use a json array instead of a nested list, updated one model to use the new model names and added an example geojson.

--- c#/Onsite-API-Example-Code/BodyExamples.cs
+++ c#/Onsite-API-Example-Code/BodyExamples.cs
@@ -11,10 +11,10 @@ namespace Onsite_API_Example_Code
         /// Example post classes used for the various endpoints
         /// </summary>
        // static PostTelematicsNodeFieldBoundaryRequest postTelematicsNodeFieldBoundary = new PostTelematicsNodeFieldBoundaryRequest { type = "FeatureCollection", features = new List<FeatureRequest> features};
-        static RawFileStorageRequest rawFileStorage = new RawFileStorageRequest { AccessKey = "", BucketName = "", Region = "", SecretKey = "", BucketPath = "" };
-        static FileRequest fileRequest = new FileRequest { NotificationIds = { }, RawFileStorage = new RawFileStorageRequest()};
+        static RawFileStorage rawFileStorage = new RawFileStorage { AccessKey = "", BucketName = "", Region = "", SecretKey = "", BucketPath = "" };
+        static FileRequest fileRequest = new FileRequest { NotificationIds = { }, RawFileStorage = new RawFileStorage()};
         static PostRequest postRequest = new PostRequest { Url = "" };
         static PostPlantingSummaryRequest postPlantingSummaryRequest = new PostPlantingSummaryRequest { StartDate = DateTime.Parse("2020-04-06T15:34:36.600Z"), EndDate = DateTime.Parse("2020-04-06T15:34:36.600Z"), CallbackURL = "" };
-        static PostNotificationSnsRequest postNotificationSnsRequest = new PostNotificationSnsRequest { ApiKey = "", SecretKey = "", TopicArn = "", Region = "" };
+        static SNSRequest postNotificationSnsRequest = new SNSRequest { ApiKey = "", SecretKey = "", TopicArn = "", Region = "" };
     }
 }
